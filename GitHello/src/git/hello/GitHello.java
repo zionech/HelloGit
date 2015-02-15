54e887993a02dd04bba0812f61919a45453a2497
zionech <sionech_0921@yahoo.com>
2015-02-15 08:27:55 +0700

    added one line
    Signed-off-by: zionech <sionech_0921@yahoo.com>

--- GitHello/src/git/hello/GitHello.java
+++ GitHello/src/git/hello/GitHello.java
@@ -2,6 +2,7 @@ package git.hello;
 
 public class GitHello {
 	  public static void main(String[] args) {
-	    System.out.println("Hello, Git is fun");
+	    System.out.println("Hello, Git is fun.");
+	    System.out.println("Though it's kinda confusing.");
 	  }
 }
